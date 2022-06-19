89ec1b627cd90fd739169037b91dc9d2226bee8d
Satvik Kishore <satvik.kishore@gmail.com>
2022-06-19 18:02:10 -0400

    postmaker online

---
+++ assets/python/postmaker.py
@@ -0,0 +1,32 @@
+import fileinput
+import json
+import shutil
+
+
+def make_single_post(template_file, variables, name):
+    newfilename = "posts/" + name + "/post.html"
+    shutil.copyfile(template_file, newfilename)
+    for var in variables:
+        print(var, variables[var])
+        with fileinput.FileInput(newfilename, inplace=True, backup=".bak") as file:
+            for line in file:
+                print(line.replace("VARIABLE::" + var, variables[var]), end="")
+                pass
+            pass
+    return 0
+
+
+def make_all_posts():
+    variables_all = json.load(open("posts/variables.json"))["posts"]
+    template_file = "posts/template.html"
+    for post in variables_all:
+        make_single_post(
+            template_file=template_file, variables=variables_all[post], name=post
+        )
+        pass
+    return 0
+
+
+if __name__ == "__main__":
+    make_all_posts()
+    pass
